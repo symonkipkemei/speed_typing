79a1e4906266a69f160bc2987f1f776e20b8d254
symonkipkemei <symonkipkemei@gmail.com>
2022-02-08 22:13:04 +0300

    updates

--- typing.py
+++ typing.py
@@ -2,6 +2,8 @@ def speed_typo():
     "Words to be typed faster"
     global count
     import random
+    from datetime import timedelta
+    from timeit import default_timer as timer
     words_1 = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
 
     print("*******SPEED TYPING GAME*****\n"
@@ -10,6 +12,7 @@ def speed_typo():
     choice = int(input("How many words do you want to attempt? : "))
     count = 0
 
+    start = timer()
     for x in range(0, choice):
         # the five letter word
         a = random.choice(words_1)
@@ -27,6 +30,8 @@ def speed_typo():
 
         if user_word == comp_word:
             count += 1
+    end = timer()
+    time_taken = timedelta(minutes=start - end)
 
     pass_mark = count / 2
 
@@ -36,7 +41,7 @@ def speed_typo():
     else:
         print("Poor. please put more effort")
 
-    print(f"You scored {count} out of {choice}")
+    print(f"You scored {count} out of {choice} in {time_taken} seconds ")
 
 
 speed_typo()
